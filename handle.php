ab74a18d7cd29fee47cf0224901175801fa53fb0
At Root-13 <parischzt@gmail.com>
2019-08-02 02:34:48 +0700

    add

--- handle.php
+++ handle.php
@@ -1,9 +1,6 @@
-function getIpAddress() {
-    if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        $ipAddresses = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-        return trim(end($ipAddresses));
-    }
-    else {
-        return $_SERVER['REMOTE_ADDR'];
-    }
+<?php
+if( $_GET["test"] |)
+{
+echo "welcome " . $+_GET['test']
 }
+?>
