d727e1e77b9829123022b96c1e1fd7f53bf5b50e
At Root-13 <parischzt@gmail.com>
2019-08-02 02:22:22 +0700

    Add files via upload

---
+++ handle.php
@@ -0,0 +1,9 @@
+function getIpAddress() {
+    if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipAddresses = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+        return trim(end($ipAddresses));
+    }
+    else {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+}
